178947a225cbac8d96774d097fc8e770bf394677
kooldzing <kooldezingebay@gmail.com>
2026-04-12 19:31:24 -0300

    Update compiler.js

--- static/js/compiler.js
+++ static/js/compiler.js
@@ -119,18 +119,18 @@ async function compileContract() {
 
     if (!result.success) {
 
-  console.error("FULL ERROR:", result);
-
-  if (result.details) {
-    result.details.forEach(err => {
-      logToTerminal(err.formattedMessage, "error");
-    });
-  } else {
-    logToTerminal(result.error || "Compilation failed", "error");
-  }
-
-  return;
-  }
+      console.error("FULL ERROR:", result);
+    
+      if (result.details) {
+        result.details.forEach(err => {
+          logToTerminal(err.formattedMessage, "error");
+        });
+      } else {
+        logToTerminal(result.error || "Compilation failed", "error");
+      }
+    
+      return;
+      }
 
     compiledContract = result;
     currentContractABI = result.abi;
